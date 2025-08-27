d81d9ab32aec6e412350e1e35c171197b33d03c6
Amila619 <amilasandeepa349@gmail.com>
2025-08-27 21:37:58 +0530

    - intial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Container } from "@mui/material"
+import Quiz from "./components/Quiz"
+
+function App() {
+  return (
+    <Container>
+      <Quiz />
+    </Container>
+  )
+}
+
+export default App
---
+++ src/components/Quiz.tsx
@@ -0,0 +1,9 @@
+import { Typography } from "@mui/material";
+
+export default function Quiz() {
+  return (
+    <>
+      <Typography>Demo Quiz App</Typography>
+    </>
+  );
+}
