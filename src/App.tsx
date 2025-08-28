3aa8b4067195f5c58405bbbca858fc6c991828b9
Amila619 <amilasandeepa349@gmail.com>
2025-08-29 00:00:11 +0530

    - added roboto font

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
-import { Container } from "@mui/material"
-import Quiz from "./components/Quiz"
+import { Container } from "@mui/material";
+import Quiz from "./components/Quiz";
 
 function App() {
+  type Question = {
+    question: string;
+    answers: string[];
+    correctAnswer: number;
+  };
+
+  const questionProps: Question = {
+    question: "What is the capital of France?",
+    answers: ["Berlin", "Madrid", "Paris", "Rome"],
+    correctAnswer: 2,
+  };
+
   return (
-    <Container>
-      <Quiz />
+    <Container
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        minHeight: "100vh",
+      }}
+    >
+      <Quiz {...questionProps} />
     </Container>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Quiz.tsx
+++ src/components/Quiz.tsx
@@ -1,9 +1,65 @@
-import { Typography } from "@mui/material";
+import { Box, Typography } from "@mui/material";
+import { useState } from "react";
 
-export default function Quiz() {
+type QuizProps = {
+  question: string;
+  answers: string[];
+};
+
+export default function Quiz({ question, answers }: QuizProps) {
+  const [selected, setSelected] = useState<number | null>(null);
+
   return (
-    <>
-      <Typography>Demo Quiz App</Typography>
-    </>
+    <Box
+      sx={{
+        maxWidth: "600px",
+        width: "full",
+        border: "1px solid",
+        borderRadius: "12px",
+        overflow: "hidden"
+      }}
+    >
+      <Typography
+        sx={{
+          color: "indigo",
+          bgcolor: "gray",
+          px: 4,
+          py: 2,
+          ":hover": { color: "black" },
+          fontSize: { xs: "1.5rem", sm: 24, md: "inherit" },
+        }}
+        variant="h3"
+      >
+        {question}
+      </Typography>
+
+      <Box
+        sx={{
+          display: "grid",
+          gap: 2,
+        }}
+      >
+        {answers.map((answer, idx) => (
+          <Box
+            key={idx}
+            onClick={() => setSelected(idx)}
+            sx={{
+              p: 1,
+              border: "1px solid",
+              borderColor: selected === idx ? "indigo" : "#ccc",
+              borderRadius: 2,
+              textAlign: "center",
+              cursor: "pointer",
+              backgroundColor: selected === idx ? "lavender" : "transparent",
+              transition: "0.3s",
+            }}
+          >
+            <Typography>
+              {answer}
+            </Typography>
+          </Box>
+        ))}
+      </Box>
+    </Box>
   );
 }
